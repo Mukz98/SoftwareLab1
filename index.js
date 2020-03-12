51fb1ef25f2f5649c040ab71e4bcbdfbb814424e
Mukz98 <thivhonali48@gmail.com>
2020-03-12 11:33:09 +0200

    Lab2

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const app = express()
+
+const mainRouter = require('./mainRoutes')
+app.use(mainRouter)
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
+
+console.log('Express server running on port 3000')
